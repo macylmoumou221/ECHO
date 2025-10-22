a63cb381cbc6dedb0b998eadbced176ca769ac6b
macylmoumou221 <macyl24601@gmail.com>
2025-10-23 00:56:33 +0200

    TRY AGAIN

--- src/components/GoogleRedirect.jsx
+++ src/components/GoogleRedirect.jsx
@@ -6,13 +6,24 @@ const GoogleRedirect = () => {
   const location = useLocation();
 
   useEffect(() => {
+    console.log("GoogleRedirect component mounted!")
+    console.log("Current location:", location)
+    console.log("Full URL:", window.location.href)
+    console.log("Search params:", location.search)
+    
     const params = new URLSearchParams(location.search);
     const token = params.get("token");
+    
+    console.log("Extracted token:", token)
+    
     if (token) {
+      console.log("Token found! Storing in localStorage...")
       localStorage.setItem("token", token);
       localStorage.setItem("userToken", token);
+      console.log("Token stored. Navigating to /acceuil...")
       navigate("/acceuil");
     } else {
+      console.log("No token found. Redirecting to login...")
       navigate('/login')
     }
   }, [location, navigate]);
